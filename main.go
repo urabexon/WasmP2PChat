6214d57123f040a1d3c2b8b74b00a43b69d395a3
urabexon <hiroki708.a48uk@gmail.com>
2025-07-29 20:45:02 +0900

    wasm compiler check

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
+//go:build js && wasm
+// +build js,wasm
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// "syscall/js"
+
+	// "github.com/pion/webrtc/v3"
+	// "github.com/urabexon/WasmP2PChat/"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	select {}
 }
