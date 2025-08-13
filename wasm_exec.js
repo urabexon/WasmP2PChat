706b53b4d0dac927f0e6f58aef1c6c9bcb4b2f88
urabexon <hiroki708.a48uk@gmail.com>
2025-08-14 08:27:40 +0900

    encoder decoder check

--- wasm_exec.js
+++ wasm_exec.js
@@ -69,6 +69,35 @@
         }
     }
 
-    
+    if (!globalThis.crypto) {
+		throw new Error("globalThis.crypto is not available, polyfill required (crypto.getRandomValues only)");
+	}
 
+    if (!globalThis.performance) {
+		throw new Error("globalThis.performance is not available, polyfill required (performance.now only)");
+	}
+
+	if (!globalThis.TextEncoder) {
+		throw new Error("globalThis.TextEncoder is not available, polyfill required");
+	}
+
+	if (!globalThis.TextDecoder) {
+		throw new Error("globalThis.TextDecoder is not available, polyfill required");
+	}
+
+	const encoder = new TextEncoder("utf-8");
+	const decoder = new TextDecoder("utf-8");
+
+    globalThis.Go = class {
+        constructor(args) {
+            this.argv = ["js"];
+			this.env = {};
+			this.exit = (code) => {
+				if (code !== 0) {
+					console.warn("exit code:", code);
+				}
+			};
+            
+        }
+    }
 })();
