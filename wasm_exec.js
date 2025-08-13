70fe9b323755715591f92acc64df5a5da1597605
urabexon <hiroki708.a48uk@gmail.com>
2025-08-14 08:22:18 +0900

    add global process

--- wasm_exec.js
+++ wasm_exec.js
@@ -55,6 +55,20 @@
     }
 
     if (!globalThis.process) {
-        
+        globalThis.process = {
+            getuid() { return -1; },
+			getgid() { return -1; },
+			geteuid() { return -1; },
+			getegid() { return -1; },
+			getgroups() { throw enosys(); },
+			pid: -1,
+			ppid: -1,
+			umask() { throw enosys(); },
+			cwd() { throw enosys(); },
+			chdir() { throw enosys(); },
+        }
     }
+
+    
+
 })();
